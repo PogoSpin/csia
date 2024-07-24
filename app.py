b97a047adaab87a5a941a510e497506c48435e18
Daniel Nyborg <daniel.sp.nyborg@gmail.com>
2024-07-24 16:52:58 +0400

    added multiple type hints and comments

--- app.py
+++ app.py
@@ -53,7 +53,7 @@ selectedStudent = None
 
 
 class PopupWindow(ctk.CTkToplevel):
-    def __init__(self, master, title = 'Popup', width = 500, height = 400):
+    def __init__(self, master: ctk.CTk, title: str = 'Popup', width: int = 500, height: int = 400):
         super().__init__(master)
         self.geometry(f'{width}x{height}')
         self.title(title)
@@ -64,7 +64,7 @@ class PopupWindow(ctk.CTkToplevel):
         self.destroy()
 
 class ConfirmationPopup(PopupWindow):
-    def __init__(self, master, message, confirmedFunc, width = 300, height = 200):
+    def __init__(self, master: ctk.CTk, message: str, confirmedFunc: callable, width: int = 300, height: int = 200):
         super().__init__(master, title = 'Confirmation', width = width, height = height)
         
         self.message = message
@@ -77,8 +77,8 @@ class ConfirmationPopup(PopupWindow):
         self.back_button.pack(pady = 20)
 
     def userConfirmed(self):
-        self.close()
         self.confirmedFunc()
+        self.close()
 
 # Dashboard
 def openDashboard(userRole):
@@ -210,7 +210,7 @@ def openDashboard(userRole):
     
     rightPanel.grid_propagate(False) # disable auto resizing
 
-    def currentTabNameSelected(): # current item selected on all tables, ie: the item selected in current table user is in
+    def currentTabNameSelected() -> str: # current item selected on all tables, ie: the item selected in current table user is in
         currentTab = tabview.get()
         if currentTab == 'Schools':
             return selectedSchool
@@ -219,7 +219,7 @@ def openDashboard(userRole):
         elif currentTab == 'Students':
             return selectedStudent
         
-    def currentTabSelected():
+    def currentTabSelected() -> ttk.Treeview:
         currentTab = tabview.get()
         if currentTab == 'Schools':
             return schoolsTable
@@ -228,28 +228,43 @@ def openDashboard(userRole):
         elif currentTab == 'Students':
             return studentsTable
 
-    def removeItem():
+    def removeItem():  # removes item and all children
         currentTab = currentTabSelected()
+
+        # if the student tab is selected u can just delete selected student
         if currentTab == studentsTable:
             databaseConn.execQuery(f"delete from students where email = '{selectedStudent}';")
 
+        # if classes table is selected, all students in that class and the class need to be deleted
         elif currentTab == classesTable:
+            # loops over all students in that class
             for emailTuple in databaseConn.resultFromQuery(f"select email from students where classid = '{selectedClass}';"):
                 email = emailTuple[0]
-                # studentsTable.delete(findRowID(studentsTable, email))
-                databaseConn.execQuery(f"delete from students where email = '{email}';")
-            databaseConn.execQuery(f"delete from classes where id = '{selectedClass}';")
+                databaseConn.execQuery(f"delete from students where email = '{email}';") # deletes that student
 
+            # after all children deleted, delete class
+            databaseConn.execQuery(f"delete from classes where id = '{selectedClass}';")
+        
+        # if school table selected, all classes and all students inside that school must be deleted first
         elif currentTab == schoolsTable:
             selectedSchoolID = databaseConn.resultFromQuery(f"select id from schools where name = '{selectedSchool}';")[0][0]
+
+            # loop over all classes in that school
             for classIdTuple in databaseConn.resultFromQuery(f"select id from classes where schoolID = '{selectedSchoolID}';"):
                 classId = classIdTuple[0]
+
+                # loop over all students in those classes
                 for emailTuple in databaseConn.resultFromQuery(f"select email from students where classid = '{classId}';"):
                     email = emailTuple[0]
-                    databaseConn.execQuery(f"delete from students where email = '{email}';")
+                    databaseConn.execQuery(f"delete from students where email = '{email}';") # deletes student
+
+                # after all students in that class r deleted, delete that class
                 databaseConn.execQuery(f"delete from classes where id = '{classId}';")
+            
+            # after all classes and all students in those classes r deleted, finally delete the entire school
             databaseConn.execQuery(f"delete from schools where name = '{selectedSchool}';")
         
+        # visually delete the selected row from the selected table
         currentTab.delete(currentTab.selection())
 
     
